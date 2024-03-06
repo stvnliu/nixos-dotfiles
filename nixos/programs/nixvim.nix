2168753ac8b297ece50480e439c5329b373590bf
Zhongheng Liu <z.liu@outlook.com.gr>
2024-03-06 17:50:19 +0200

    03/06/24::17:50:19 autogen nixos config

--- nixos/programs/nixvim.nix
+++ nixos/programs/nixvim.nix
@@ -1,29 +1,36 @@
 { pkgs, lib, ... }:
 let
-  nixvim = import (builtins.fetchGit {
-    url = "https://github.com/nix-community/nixvim";
+nixvim = import (builtins.fetchGit {
+		url = "https://github.com/nix-community/nixvim";
 # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-ref = "nixos-23.11";
-                });
+		ref = "nixos-23.11";
+		});
 in
-  {
-    imports = [
-      nixvim.nixosModules.nixvim
-    ];
+{
+	imports = [
+		nixvim.nixosModules.nixvim
+	];
 
-    programs.nixvim = {
-      enable = true;
-      options = {
-        number = true;
-        relativenumber = true;
-        shiftwidth = 8;
-      };
-      extraPlugins = with pkgs.vimPlugins; [
-        gruvbox
-      ];
-      colorschemes.gruvbox.enable = true;
-      plugins = {
-        lightline.enable = true;
-      };
-    };
-  }
+	programs.nixvim = {
+		enable = true;
+		options = {
+			number = true;
+			relativenumber = true;
+			shiftwidth = 8;
+		};
+		extraPlugins = with pkgs.vimPlugins; [
+			gruvbox
+		];
+		colorschemes.gruvbox.enable = true;
+		plugins = {
+			lightline.enable = true;
+			lsp = {
+				enable = true;
+				servers = {
+					tsserver.enable = true;
+					lua-ls.enable = true;
+				};
+			};
+		};
+	};
+}
