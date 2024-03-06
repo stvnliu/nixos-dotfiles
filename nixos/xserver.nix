49e4fc349ee8169308a17cb112838a8b1f9bcdad
Zhongheng Liu <z.liu@outlook.com.gr>
2024-03-06 15:51:57 +0200

    03/06/24::15:51:57 autogen nixos config

--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -3,7 +3,10 @@
 	services.xserver = {
 		videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 		displayManager = {
-			sessionCommands = ''xset r rate 250 50'';
+			sessionCommands = ''
+			xset r rate 250 50
+			xrandr --output HDMI-1 --same-as eDP-1
+			'';
 		};
 		windowManager = {
 			dwm = {
