ef15b5d2da57dbf9c454f3b7c45d768026c5d280
Zhongheng Liu <z.liu@outlook.com.gr>
2024-03-06 18:37:15 +0200

    03/06/24::18:37:15 v51 autogen nixos config

--- nixos-deploy.sh
+++ nixos-deploy.sh
@@ -3,6 +3,6 @@ cd ./nixos
 sudo stow . -t /etc/nixos/
 sudo nixos-rebuild switch
 git add -A
-git commit -m "$@$(date +"%D::%H:%M:%S") v$(sudo `nix-env -p /nix/var/nix/profiles/system --list-generations | tail -1 | awk '{print $1}'`) autogen nixos config"
+git commit -m "$@$(date +"%D::%H:%M:%S") v$(sudo nix-env -p /nix/var/nix/profiles/system --list-generations | tail -1 | awk '{print $1}') autogen nixos config"
 git push
 cd ..
