56e4c71654a11aabe05124ad4105f1157a17ea58
Zhongheng Liu <z.liu@outlook.com.gr>
2024-04-19 22:14:20 +0300

    dev: setup init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+
+{
+  description = "An example NixOS configuration";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nur = { url = "github:nix-community/NUR"; };
+  };
+
+  outputs = inputs:
+    /* ignore:: */ let ignoreme = ({config,lib,...}: with lib; { system.nixos.revision = mkForce null; system.nixos.versionSuffix = mkForce "pre-git"; }); in
+  {
+    nixosConfigurations = {
+
+      mysystem = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          /* ignore */ ignoreme # ignore this; don't include it; it is a small helper for this example
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
+
