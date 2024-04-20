f5d24ff5318d34b1fd7ebdf8901d06d791e0bb4b
Zhongheng Liu <z.liu@outlook.com.gr>
2024-04-20 12:11:55 +0300

    feat!: flatten userprogs

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,10 +1,14 @@
-{...}:
+{lib, ...}:
 {
 	imports = [
 		./development.nix
+		./jdk.nix
 		./misc.nix
+		./nix-ld.nix
+		./steam.nix
 		./tex.nix
 		./utils.nix
+		./zsh.nix
 		./desktop
 		./editors
 		./terminals
