a606e22805ac768aa57f8d839e13bab9d6675dfc
Zhongheng Liu <z.liu@outlook.com.gr>
2024-04-21 13:42:25 +0300

    feat: openvpn setup

--- modules/nixos/services/openvpn.nix
+++ modules/nixos/services/openvpn.nix
@@ -1,6 +1,9 @@
 {...}:
 {
 	services.openvpn.servers = {
-		nl = { config = '' config /home/zhonghengl/openvpn/nl-free-1.ovpn ''; };
+		nl = {
+			config = '' config /home/zhonghengl/openvpn/nl-free-1.ovpn '';
+			updateResolvConf = true;
+		};
 	};
 }
