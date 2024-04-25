318be93b27da16defb3c1edc2ca92a6532f77844
Zhongheng Liu <z.liu@outlook.com.gr>
2024-04-25 22:38:21 +0300

    feat(config): networking open tcp port for debug

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,7 +59,7 @@
 	};
 	
 # Open ports in the firewall.
-	networking.firewall.allowedTCPPorts = [ ];
+	networking.firewall.allowedTCPPorts = [ 8080];
 	networking.firewall.allowedUDPPorts = [ ];
 # Or disable the firewall altogether.
 # networking.firewall.enable = false;
