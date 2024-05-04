6f2bab8b996751b175dfe85ec53213cb1f2216bd
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-04 16:14:38 +0300

    chore: rm impure flag in homeupdate

--- modules/home-manager/shells/aliases/default.nix
+++ modules/home-manager/shells/aliases/default.nix
@@ -3,6 +3,6 @@
 	cat = "${pkgs.bat}/bin/bat";
 	ll = "ls -l";
 	osupdate = "${pkgs.nh}/bin/nh os switch";
-	homeupdate = "${pkgs.nh}/bin/nh home switch -- --impure";
+	homeupdate = "${pkgs.nh}/bin/nh home switch";
 	batmon = "watch -n0 upower -i /org/freedesktop/UPower/devices/battery_BAT1";
 }
