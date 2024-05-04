d93731019922005cdeeea17a761a2072d023dea8
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-04 16:13:30 +0300

    chore: fix bug in conf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -62,7 +62,7 @@
 			commit.gpgsign = true;
 			gpg.format = "ssh";
 			gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-			user.signingkey = ./zhonghengl_ed25519.pub;
+			user.signingkey = "~/.ssh/id_ed25519.pub";
 			credential.helper = "${
 				pkgs.git.override { withLibsecret = true; }
 			}/bin/git-credential-libsecret";
