f43f0f9d0337d180c7f95d051c3431a23aa2c43a
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-06 15:32:13 +0300

    feat(games): add openttd and patches

--- modules/home-manager/desktop/games.nix
+++ modules/home-manager/desktop/games.nix
@@ -1,10 +1,15 @@
 {config, lib, pkgs, ...}:
 {
 	home.packages = with pkgs; [
+		# launchers
 		prismlauncher
 		lutris
 		heroic
+		
+		# linux games
 		torcs
 		flightgear
+		openttd
+		openttd-jgrpp
 	];
 }
