f5027c80127349cb2dec480905f9ad07babb34b6
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-06 15:29:09 +0300

    feat(web): appended nginx setup

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -3,7 +3,20 @@
 	services.nginx = {
 		enable = true;
 		virtualHosts."localhost" = {
+			serverName = "";
 			root = "/var/www/localhost";
+			locations = {
+				"/" = {
+					root = "/srv/public";
+					extraConfig = ''
+						autoindex on;
+					'';
+				};
+			};
+			listenAddresses = [
+				"127.0.0.1"
+				"[::1]"
+			];
 		};
 	};
 	networking.firewall.allowedTCPPorts = [ 80 ];
