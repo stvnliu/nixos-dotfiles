32bae6163574c103a9e69a17cffb00ca9d824d94
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-11 20:26:01 +0300

    feat: PRIME Sync config working

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -1,44 +1,31 @@
-{config, ...}: 
+{config, lib, ...}: 
 {
+	specialisation = {
+		powersave.configuration = {
+			hardware.nvidia.prime = {
+				offload = {
+					enable = lib.mkForce true;
+					enableOffloadCmd = lib.mkForce true;
+				};
+				sync.enable = lib.mkForce false;
+			};
+		};
+	};
 	hardware.nvidia = {
 		prime = {
-			offload = {
+			offload.enable = false;
+			sync = {
 				enable = true;
-				enableOffloadCmd = true;
 			};
-			# TODO Replace with your own Bus IDs
 			intelBusId = "PCI:0:2:0";
 			nvidiaBusId = "PCI:1:0:0";
 		};
-# Modesetting is required.
-		modesetting.enable = true;
-
-# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-# Enable this if you have graphical corruption issues or application crashes after waking
-# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-# of just the bare essentials.
-		powerManagement.enable = false;
-
-# Fine-grained power management. Turns off GPU when not in use.
-# Experimental and only works on modern Nvidia GPUs (Turing or newer).
-		powerManagement.finegrained = false;
-
-# Use the NVidia open source kernel module (not to be confused with the
-# independent third-party "nouveau" open source driver).
-# Support is limited to the Turing and later architectures. Full list of 
-# supported GPUs is at: 
-# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-# Only available from driver 515.43.04+
-# Currently alpha-quality/buggy, so false is currently the recommended setting.
-		open = false;
-
-# Enable the Nvidia settings menu,
-# accessible via `nvidia-settings`.
-		nvidiaSettings = true;
-
 		forceFullCompositionPipeline = true;
-
-# Optionally, you may need to select the appropriate driver version for your specific GPU.
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		powerManagement.finegrained = false;
+		open = false;
+		nvidiaSettings = true;
 		package = config.boot.kernelPackages.nvidiaPackages.stable;
 	};
 }
