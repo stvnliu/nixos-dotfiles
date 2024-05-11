ec644ea5ee233e30341c40ee8708250127a4ff2b
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-11 20:04:16 +0300

    feat: add experimental nvidia forceFullCompositionPipeline

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -36,6 +36,8 @@
 # accessible via `nvidia-settings`.
 		nvidiaSettings = true;
 
+		forceFullCompositionPipeline = true;
+
 # Optionally, you may need to select the appropriate driver version for your specific GPU.
 		package = config.boot.kernelPackages.nvidiaPackages.stable;
 	};
