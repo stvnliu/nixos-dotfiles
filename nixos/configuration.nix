0677a0f3277aeaa0ad7b03f68a871b755a852ba6
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-29 12:39:16 +0300

    feat(virt/virt-manager): use virt-manager in setup to configure and run VMs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,6 +7,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./virtualisation.nix
     ../modules/nixos
     ./specialisations
     ../overlays/test-overlay.nix
